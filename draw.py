c613026ef24fe4c5bbc0fbfaedab276e0c84a055
William <hahayes2@yahoo.com>
2020-01-06 00:00:00 -0500

    updates

---
+++ draw.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import datetime
+def draw(startdate):
+    os.system("""          
+    python pogchamp.py
+    git add .
+    git commit -m "updates" --date=format:iso8601:"{data}"
+    """.format(data=startdate))
+
+def make_dates(startdate):
+    date = datetime.datetime.strptime(startdate, "%Y-%m-%d")
+    #start on a monday 
+    dates = [str(date)]
+
+    gaps = [1 for _ in range(4)] + [2] + [1 for _ in range(13)] + [2] + [1 for _ in range(2)]
+    for g in gaps:
+        date = date + datetime.timedelta(days=g)
+        dates.append(str(date))
+        #date = date + datetime.timedelta(days=6)
+    return dates
+if __name__ == '__main__':
+    data = sys.argv[1]
+    dates = make_dates(data)
+    for d in dates:
+        draw(d)
+
+
---
+++ pogchamp.py
@@ -0,0 +1,3 @@
+with open('hahaedgyjoke.txt','a')as f:
+    f.write("pogchamp\n")
+
